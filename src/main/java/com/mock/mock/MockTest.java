92e2ce778e0304f7964d8b0bb3afde7569c1ee65
mith009 <mithileshkrooo@gmail.com>
2025-04-14 21:29:11 +0530

    stream

--- src/main/java/com/mock/mock/MockTest.java
+++ src/main/java/com/mock/mock/MockTest.java
@@ -1,7 +1,18 @@
 package com.mock.mock;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MockTest {
+
+
     public static void main(String[] args) {
-        System.out.println("hello world");
+        List<String> name= Arrays.asList("mith","Ayush","vivek","moin","firoz","washim","murli");
+
+
+
+        name.stream().filter(e->e.toLowerCase().startsWith("m")).forEach(System.out::println); //find the name that start with "m"
+      //  System.out.println("hello world");
+
     }
 }
